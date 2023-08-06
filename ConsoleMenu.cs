3fe7143c68b0b58d573a8883448ebc53fe0b510d
Konstantin <118442934+MonsterCreator@users.noreply.github.com>
2023-08-06 12:08:40 +0700

    Added ConsoleProgressBar class

--- ConsoleMenu.cs
+++ ConsoleMenu.cs
@@ -11,25 +11,41 @@ namespace ConsoleAddonLibrary
     {
         private List<MenuElement> menuElements = new List<MenuElement>();
 
-        public string _menuName;
+        public string MenuName { get; set; }
+
+        private ConsoleColor _boxColor = ConsoleColor.Gray;
         private int _selectedElement = 0;
+        private int _verticalCursorPos = 0;
+        private bool _isShown = false;
 
-        public void AddMenuElement(MenuElement element)
-        {
-            menuElements.Add(element);
-        }
+        private bool CloseMenu = false;
 
-        public void ShowMenu()
+        public void AddMenuElement(MenuElement element) { menuElements.Add(element); }
+
+        public void RemoveMenuElement(int index) { menuElements.Remove(menuElements[index]); }
+
+        public void ShowMenu(bool ShowExitButton = false)
         {
+            if (ShowExitButton) { AddMenuElement(new MenuElement("Close menu", () => Exit())); }
+
             PrintMenu();
             while (true)
             {
                 ConsoleKey key = Console.ReadKey().Key;
-
                 switch (key)
                 {
                     case ConsoleKey.Enter:
-                        menuElements[_selectedElement].Method.Invoke(); break;
+                        menuElements[_selectedElement].Method.Invoke();
+                        if (!CloseMenu)
+                        {
+                            ConsoleLogs.SystemLog("Press any key for continue");
+                            Console.ReadKey();
+                            Console.Clear();
+                            _isShown = false;
+                            PrintMenu();
+                        }
+                        else Console.Clear();
+                        break;
                     case ConsoleKey.S:
                         if (_selectedElement < menuElements.Count - 1)
                         {
@@ -49,6 +65,7 @@ namespace ConsoleAddonLibrary
                         {
                             _selectedElement--;
                             PrintMenu();
+                            
                         }
                         break;
                     case ConsoleKey.UpArrow:
@@ -59,28 +76,42 @@ namespace ConsoleAddonLibrary
                         }
                         break;
                 }
+
+                if (CloseMenu)
+                {
+                    RemoveMenuElement(menuElements.Count - 1);
+                    CloseMenu = false;
+                    _isShown = false;
+                    return;
+                }
             }
         }
 
         private void PrintMenu()
         {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = _boxColor;
             string line = "";
-            for (int i = 0; i < FindMaxLenght() + 2; i++)
+            for (int i = 0; i < FindMaxLenght() + 4; i++)
             {
-                line += "-";
-            }
-            Console.WriteLine(line);
-            if (_menuName != null)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkYellow;
-                PrintLine(_menuName);
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine(line);
+                line += "█";
             }
             
 
+            if (!_isShown)
+            {
+                Console.WriteLine(line);
+                if (MenuName != null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    PrintLine(MenuName);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine(line);
+                    _verticalCursorPos = Console.CursorTop;
+                }
+                _isShown = true;
+            }
+
+            Console.CursorTop = _verticalCursorPos;
             if (menuElements != null)
             {
                 for (int i = 0; i < menuElements.Count; i++)
@@ -90,35 +121,32 @@ namespace ConsoleAddonLibrary
                         ConsoleLogs.PrintSelected(() =>
                         {
                             PrintLine(menuElements[i].ElementName);
-                            
                         });
                     }
                     else
                     {
                         PrintLine(menuElements[i].ElementName);
                     }
-                    
                 }
+                Console.WriteLine(line);
             }
         }
 
         private void PrintLine(string elmentName)
         {
             int maxLenght = FindMaxLenght();
-            Console.Write("|");
+            Console.Write("█ ");
             Console.ForegroundColor= ConsoleColor.Yellow;
             Console.Write(elmentName);
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = _boxColor;
             string next = "";
             for (int i = 0; i < maxLenght - elmentName.Length; i++)
             {
                 next += " ";
             }
-            next += "|";
+            next += " █";
             Console.Write(next);
             Console.WriteLine();
-
-
         }
 
         private int FindMaxLenght()
@@ -132,6 +160,11 @@ namespace ConsoleAddonLibrary
                 }
             }
             return maxLenght;
+        }
+
+        private void Exit()
+        {
+            CloseMenu = true;
         }
     }
 
---
+++ ConsoleProgressBar.cs
@@ -0,0 +1,119 @@
+﻿using ConsoleAddonLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAddonLibrary
+{
+    public class ConsoleProgressBar
+    {
+        public ConsoleProgressBar(int endValue)
+        {
+            _endValue = endValue;
+        }
+
+        public int BarLenght = 30;
+
+        private int _verticalCursorPos;
+        private bool _isShown = false;
+
+        private string _borderUp = string.Empty;
+        private string _borderDown = string.Empty;
+
+        private int _endValue;
+        private int _value = 0;
+        public int Value
+        {
+            get { return _value; }
+            set { 
+                _value = value;
+                CheckIsFinished();
+                }
+        }
+
+        public void Show()
+        {
+            UpdateBorders();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine(_borderUp);
+            _verticalCursorPos = Console.CursorTop;
+            Console.Write("║ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            string bar = string.Empty;
+            for (int i = 0; i < BarLenght; i++)
+            {
+                bar += "#";
+            }
+
+            Console.Write(bar);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" ║ ");
+            Console.WriteLine(_borderDown);
+            _isShown = true;
+        }
+
+        private void UpdateBar()
+        {
+            if (_isShown)
+            {
+                Console.CursorTop = _verticalCursorPos;
+                Console.CursorLeft = 2;
+
+                int lenght = GetFinishedElements(_value, _endValue);
+                string bar = string.Empty;
+                for (int i = 0; i < lenght; i++)
+                {
+                    bar += "█";
+                }
+                Console.Write(bar);
+            }
+        }
+
+
+        private void UpdateBorders()
+        {
+            _borderUp = string.Empty;
+            _borderUp += "╔";
+            for (int i = 0; i < BarLenght + 2; i++)
+            {
+                _borderUp += "═";
+            }
+            _borderUp += "╗";
+
+            _borderDown = string.Empty;
+            _borderDown += "╚";
+            for (int i = 0; i < BarLenght + 2; i++)
+            {
+                _borderDown += "═";
+            }
+            _borderDown += "╝";
+        }
+
+        private void CheckIsFinished()
+        {
+            if (_value >= _endValue)
+            {
+                ConsoleLogs.SystemLog("Task Copleted");
+            }
+            else
+            {
+                UpdateBar();
+            }
+        }
+
+        public int GetFinishedElements(int completedTasks, int totalTasks)
+        {
+            // Определяем процент выполненных задач.
+            int percentComplete = (int)Math.Round(((double)completedTasks / (double)totalTasks) * 100.0);
+
+            // Вычисляем количество элементов, которые должны быть выведены.
+            int numElements = (int)Math.Round(((double)percentComplete / 100.0) * BarLenght);
+
+            return numElements;
+        }
+    }
+}
